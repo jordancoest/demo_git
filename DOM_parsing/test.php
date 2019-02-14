c7b899feffc00f284266396f2145ca1f32f6fa8a
aymanagh <47530849+aymanagh@users.noreply.github.com>
2019-02-14 13:51:52 +0100

    Update test.php

--- DOM_parsing/test.php
+++ DOM_parsing/test.php
@@ -1,14 +1,29 @@
+
 <?php
 
-    require_once 'simple_html_dom.php';
+    require_once './simplehtmldom_1_7/simple_html_dom.php';
 
-	$html = file_get_html('http://www.emeraldgrouppublishing.com/authors/writing/calls.htm?id=8131');
+	$html = file_get_html('http://www.emeraldgrouppublishing.com/authors/writing/calls.htm?id=7874');
 
 	$text = "";
 	foreach($html->find('div[id="pgSectionCn"] p') as $e){
 	    //echo "div ".$e->innertext  . '<br>';
 	    $text = $text.$e->innertext;
 	}
-	preg_match("`(Special issue (.*))`", $text, $res_regex);
-	echo $res_regex[2];
+
+
+
+	$debut = $html->find('div[id="pgSectionCn"] p', 1);
+
+	echo $debut;
+	//$debut = strip_tags($debut);
+	//$text = strip_tags($text);
+
+	//$rest = preg_replace($debut, $text);
+
+	//**/$rest = substr($text, 0, 1000);
+/*
+	echo $debut;
+	echo "<br><br><br>";
+	echo $text;*/
 ?>
