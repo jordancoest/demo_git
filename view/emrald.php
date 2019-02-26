2e234634e29e74cd606683403d791a1c84f1920c
aymanagh <47530849+aymanagh@users.noreply.github.com>
2019-02-26 12:50:48 +0100

    Update emrald.php

--- view/emrald.php
+++ view/emrald.php
@@ -7,7 +7,7 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 echo "<p><b>Appel à publication Emerald</b></p>";
 
-$sql = "SELECT titreAppel, resume, dateFinSoumission, datePublication, lien FROM appelAPublication";
+$sql = "SELECT titreAppel, resume, dateFinSoumission, datePublication, lien, idRevue FROM appelAPublication";
 $result = $conn->query($sql);
 
 echo "<table border='1px'>";
@@ -16,49 +16,9 @@ echo "<tr><th>titreAppel</th><th>resume</th><th>dateFinSoumission</th><th>datePu
 while($row = $result->fetch(PDO::FETCH_ASSOC)){   //Creates a loop to loop through results
     echo "<tr><td>" . $row['titreAppel'] . "</td><td>" . $row['resume'] ."</td><td>".$row['dateFinSoumission']."</td><td>".$row['datePublication']."</td>
         <td><a href=".$row['lien'].">lien</a></td>
-        <td><a href=emrald.php?f=".$row['lien'].">apercu annonce</a></td></tr>";  //$row['index'] the index here is a field nam
+        <td><a href=emrald.php?r=".$row['idRevue'].">Revue</a></td></tr>";  //$row['index'] the index here is a field nam
 }
 
 echo "</table>"; //Close the table in HTML
 $conn = null;
-
-
-$fichier = $_GET['f'];
-require_once './simplehtmldom_1_7/simple_html_dom.php';
-    
-$html = file_get_html($fichier);
-
-$text = ""; 
-foreach($html->find('div[id="pgSectionCn"] p') as $e){
-    //echo "div ".$e->innertext  . '<br>';
-    $text = $text.$e->innertext;
-}
-$text = strip_tags_content($text);
-$text = strip_tags($text);
-$text = str_replace("&nbsp;", " ", $text);
-$rest = substr($text, 0, 1000);
-echo "<script type='text/javascript'>
-alert('$rest');
-</script>";
-
-
-
-function strip_tags_content($text, $tags = '', $invert = FALSE) {
-
-  preg_match_all('/<(.+?)[\s]*\/?[\s]*>/si', trim($tags), $tags);
-  $tags = array_unique($tags[1]);
-   
-  if(is_array($tags) AND count($tags) > 0) {
-    if($invert == FALSE) {
-      return preg_replace('@<(?!(?:'. implode('|', $tags) .')\b)(\w+)\b.*?>.*?</\1>@si', '', $text);
-    }
-    else {
-      return preg_replace('@<('. implode('|', $tags) .')\b.*?>.*?</\1>@si', '', $text);
-    }
-  }
-  elseif($invert == FALSE) {
-    return preg_replace('@<(\w+)\b.*?>.*?</\1>@si', '', $text);
-  }
-  return $text;
-} 
 ?>
